583b9523ff1cf0e28eb1f9c747de683c9b204070
oozoofrog <oozoofrog@gmail.com>
2026-03-07 02:57:05 +0900

    Initial commit

---
+++ Days/Days/DaysApp.swift
@@ -0,0 +1,32 @@
+//
+//  DaysApp.swift
+//  Days
+//
+//  Created by oozoofrog on 3/7/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DaysApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Days/DaysTests/DaysTests.swift
@@ -0,0 +1,17 @@
+//
+//  DaysTests.swift
+//  DaysTests
+//
+//  Created by oozoofrog on 3/7/26.
+//
+
+import Testing
+@testable import Days
+
+struct DaysTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
